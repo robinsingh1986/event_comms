52f4f8c7abf9d193187825b4d6a045b13cc07f31
Robin Singh <er.robinsingh1986@gmail.com>
2023-11-30 18:15:57 -0500

    First copy of the code in boatSearch

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class BoatSearch extends LightningElement {}
+ // imports
+ export default class BoatSearch extends LightningElement {
+    isLoading = false;
+    
+    // Handles loading event
+    handleLoading() { }
+    
+    // Handles done loading event
+    handleDoneLoading() { }
+    
+    // Handles search boat event
+    // This custom event comes from the form
+    searchBoats(event) { }
+    
+    createNewBoat() { }
+  }
+  
